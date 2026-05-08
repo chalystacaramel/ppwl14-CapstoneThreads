759f9f8e416bb973f42bacf4bda452c50ef2addb
Chalysta Setyani <chalystasetyani9@gmail.com>
2026-05-08 11:47:07 +0700

    feat: setup elysia server

--- apps/be/index.ts
+++ apps/be/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .get("/", () => "Threads Clone API is running!")
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);

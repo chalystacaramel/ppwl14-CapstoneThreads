a7f3a3e17f4a842282cbd1d8fb28224e3aa48dca
adheliaissabel <h1101241001@student.untan.ac.id>
2026-05-18 14:07:34 +0700

    fix login

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,21 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
-
-function HomePage() {
-  return <div style={{ color: '#fff', padding: 24 }}>🏠 Beranda — dikerjakan Aisyah</div>
-}
-function FormPostPage() {
-  return <div style={{ color: '#fff', padding: 24 }}>📝 Form Post — dikerjakan Andy</div>
-}
-function NotifPage() {
-  return <div style={{ color: '#fff', padding: 24 }}>🔔 Notifikasi — dikerjakan Iqlima</div>
-}
+import Feed from './pages/Feed'
+import FormPostPage from './pages/FormPostPage'
+import NotifPage from './pages/NotifPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Feed />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/post" element={<FormPostPage />} />
         <Route path="/notifications" element={<NotifPage />} />
